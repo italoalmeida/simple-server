a946bad4641fb344ced6938b2ad15f3ef270d59d
italo.almeida <italo.almeida04@gmail.com>
2024-09-22 17:25:46 -0300

    creates server

---
+++ index.js
@@ -0,0 +1,18 @@
+// Importando o Express
+const express = require("express");
+
+// Inicializando a aplicação Express
+const app = express();
+
+// Definindo a porta
+const PORT = 3000;
+
+// Definindo a rota raíz "/"
+app.get("/", (req, res) => {
+  res.send("UP"); // Retorna a mensagem "UP"
+});
+
+// Iniciando o servidor na porta definida
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
